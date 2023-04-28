6681a6b73efbb8219ed7834504d476ddbac7699a
Wagner da Silva Aquino Junior <wagner.aquino@tecnospeed.com.br>
2023-04-27 21:54:39 -0300

    digitando valores na tela e apagando

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-let valorTela = document.querySelector(".tela");
+let tela = document.querySelector(".tela");
+let algarismo = "";
+let simbolo = "";
+let calculo = 0;
 
 botoes = document.querySelectorAll(".botao");
 
 botoes.forEach((botao) => {
     botao.addEventListener("click", (e) => {
-        const textoBotao = e.target.textContent
-        console.log(textoBotao)
+        const textoBotao = e.target.textContent;
+        inserirNaTela(textoBotao);
+        //console.log(textoBotao);
     })
 })
-//valorTela.innerHTML = 10 + 5
+
+
+function inserirNaTela (caractere) {
+    if (caractere >= 0 && caractere <= 9){
+        algarismo = algarismo + caractere;
+        calculo = algarismo;
+        tela.innerHTML = algarismo;
+    } else if (caractere === "C") {
+        calculo = 0;
+        algarismo = "";
+        tela.innerHTML = calculo;
+    } else if (caractere === "←") {
+        algarismo = algarismo.slice(0, -1);
+        if (algarismo === ""){
+            tela.innerHTML = 0;
+        } else {
+            tela.innerHTML = algarismo;
+        }
+    }
+}
